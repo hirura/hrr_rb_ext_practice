d72244dd5d6117c300a786c93d13b2c167dc88d6
hirura <hirura@gmail.com>
2020-02-29 10:45:54 +0900

    add singleton, instance, and private method

--- ext/hrr_rb_ext_practice/hrr_rb_ext_practice.c
+++ ext/hrr_rb_ext_practice/hrr_rb_ext_practice.c
@@ -2,8 +2,29 @@
 
 VALUE rb_cHrrRbExtPractice;
 
+VALUE
+singleton_method(VALUE self)
+{
+  return rb_str_new2("singleton method");
+}
+
+VALUE
+instance_method(VALUE self)
+{
+  return rb_str_new2("instance method");
+}
+
+VALUE
+private_method(VALUE self)
+{
+  return rb_str_new2("private method");
+}
+
 void
 Init_hrr_rb_ext_practice(void)
 {
   rb_cHrrRbExtPractice = rb_define_class("HrrRbExtPractice", rb_cObject);
+  rb_define_singleton_method(rb_cHrrRbExtPractice, "singleton_method", singleton_method, 0);
+  rb_define_method(rb_cHrrRbExtPractice, "instance_method", instance_method, 0);
+  rb_define_private_method(rb_cHrrRbExtPractice, "private_method", private_method, 0);
 }
--- spec/hrr_rb_ext_practice_spec.rb
+++ spec/hrr_rb_ext_practice_spec.rb
@@ -6,4 +6,38 @@ RSpec.describe HrrRbExtPractice do
   it "is instantiatable" do
     expect(described_class.new.class).to eq(described_class)
   end
+
+  describe ".singleton_method" do
+    it "returns a string" do
+      expect(described_class.singleton_method).to eq("singleton method")
+    end
+  end
+
+  describe "#instance_method" do
+    it "returns a string" do
+      expect(described_class.new.instance_method).to eq("instance method")
+    end
+  end
+
+  describe "#private_method" do
+    context "when called as an instance method" do
+      it "raises NoMethodError" do
+        expect{described_class.new.private_method}.to raise_error NoMethodError
+      end
+    end
+
+    context "when called within the context of self" do
+      let(:instance){
+        instance = described_class.new
+        def instance.call_private_method
+          private_method
+        end
+        instance
+      }
+
+      it "returns a string" do
+        expect(instance.call_private_method).to eq("private method")
+      end
+    end
+  end
 end
